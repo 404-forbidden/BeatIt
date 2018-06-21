fba797dc2d849306630dca28275eefd45203a2c2
Seonwoo <nsw6662@gmail.com>
2018-06-21 20:58:16 +0900

    Initial commit

---
+++ Beat It/src/beat_it_1/Track.java
@@ -0,0 +1,51 @@
+package beat_it_1;
+
+public class Track {
+	private String trackName;
+	private String trackImage;
+	private String selectedMusic; // 선택시 나오는 하이라이트
+	private String trackMusic; // 게임 플레이 중 나오는 음악
+	
+	public String getTrackName() {
+		return trackName;
+	}
+	public void setTrackName(String trackName) {
+		this.trackName = trackName;
+	}
+	public String getTrackImage() {
+		return trackImage;
+	}
+	public void setTrackImage(String trackImage) {
+		this.trackImage = trackImage;
+	}
+	public String getSelectedMusic() {
+		return selectedMusic;
+	}
+	public void setSelectedMusic(String selectedMusic) {
+		this.selectedMusic = selectedMusic;
+	}
+	public String getTrackMusic() {
+		return trackMusic;
+	}
+	public void setTrackMusic(String trackMusic) {
+		this.trackMusic = trackMusic;
+	}
+	
+	public Track(String trackName) {
+		super();
+		this.trackName = trackName;
+		this.trackImage = trackName + ".jpg";
+		this.selectedMusic = trackName + " Selected.mp3";
+		this.trackMusic = trackName + ".mp3";
+	}
+	
+	public Track(String trackName, String trackImage, String selectedMusic, String trackMusic, String trackButton) {
+		super();
+		this.trackName = trackName;
+		this.trackImage = trackImage;
+		this.selectedMusic = selectedMusic;
+		this.trackMusic = trackMusic;
+	}
+	
+	
+}
