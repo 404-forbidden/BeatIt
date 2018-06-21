f99ed9f99a389fa03ccc682b97a589ef5980d980
Seonwoo <nsw6662@gmail.com>
2018-06-21 23:09:36 +0900

    edit design

--- Beat It/src/beat_it_1/BeatIt.java
+++ Beat It/src/beat_it_1/BeatIt.java
@@ -7,11 +7,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -25,30 +22,29 @@ public class BeatIt extends JFrame {
 	private Graphics screenGraphic;
 	
 	private Image background = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
-	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
 	private Image gameInfoImage = new ImageIcon(Main.class.getResource("../images/gameInfo.png")).getImage();
 	private Image judgementLineImage = new ImageIcon(Main.class.getResource("../images/judgementLine.png")).getImage();
 	private Image noteRouteImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
 	private Image noteRouteLineImage = new ImageIcon(Main.class.getResource("../images/noteRouteLine.png")).getImage();
 	private Image noteBasicImage = new ImageIcon(Main.class.getResource("../images/note.jpg")).getImage();
 	
-	private ImageIcon startButtonBasicImage = new ImageIcon(Main.class.getResource("../images/startButtonBasic.png"));
+	private ImageIcon startButtonImage = new ImageIcon(Main.class.getResource("../images/startButton.png"));
 	private ImageIcon startButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/startButtonEntered.png"));
-	private ImageIcon quitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/quitButtonBasic.png"));
+	private ImageIcon quitButtonImage = new ImageIcon(Main.class.getResource("../images/quitButton.png"));
 	private ImageIcon quitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/quitButtonEntered.png"));
+	private ImageIcon quitButtonSquareImage = new ImageIcon(Main.class.getResource("../images/quitButtonSquare.png"));
+	private ImageIcon quitButtonSquareEnteredImage = new ImageIcon(Main.class.getResource("../images/quitButtonSquareEntered.png"));
 	
-	private ImageIcon easyButtonBasicImage = new ImageIcon(Main.class.getResource("../images/easyButtonBasic.png"));
+	private ImageIcon easyButtonImage = new ImageIcon(Main.class.getResource("../images/easyButton.png"));
 	private ImageIcon easyButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/easyButtonEntered.png"));
-	private ImageIcon hardButtonBasicImage = new ImageIcon(Main.class.getResource("../images/hardButtonBasic.png"));
+	private ImageIcon hardButtonImage = new ImageIcon(Main.class.getResource("../images/hardButton.png"));
 	private ImageIcon hardButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/hardButtonEntered.png"));
 	
-	private JButton startButton = new JButton(startButtonBasicImage);
-	private JButton quitButton = new JButton(quitButtonBasicImage);
-	private JButton easyButton = new JButton(easyButtonBasicImage);
-	private JButton hardButton = new JButton(hardButtonBasicImage);
-	private JButton backButton = new JButton("esc");
-	
-	private int mouseX, mouseY;
+	private JButton startButton = new JButton(startButtonImage);
+	private JButton quitButton = new JButton(quitButtonImage);
+	private JButton easyButton = new JButton(easyButtonImage);
+	private JButton hardButton = new JButton(hardButtonImage);
+	private JButton backButton = new JButton(quitButtonSquareImage);
 	
 	private boolean isMainScreen = false;
 	private boolean isGameScreen = false;
@@ -57,6 +53,7 @@ public class BeatIt extends JFrame {
 	private JButton trackButton0 = new JButton("<html>Emotional Future Bass<br>- AlexZavesa</html>");
 	private JButton trackButton1 = new JButton("<html>Future Dubstep<br>- Marco Margna</html>");
 	private JButton trackButton2 = new JButton("<html>Raw Power<br>- Vicate Studio</html>");
+	private JLabel selectedLabel = new JLabel();
 
 	private Image selectedImage;
 	private Music selectedMusic;
@@ -80,9 +77,15 @@ public class BeatIt extends JFrame {
 		trackList.add(new Track("Future Dubstep"));
 		trackList.add(new Track("Raw Power"));
 		
-		makeTrackButton(trackButton0, 0, -175);
-		makeTrackButton(trackButton1, 1, -50);
-		makeTrackButton(trackButton2, 2, 75);
+		makeTrackButton(trackButton0, 0);
+		makeTrackButton(trackButton1, 1);
+		makeTrackButton(trackButton2, 2);
+		
+		selectedLabel.setBounds(Main.SCREEN_WIDTH - 400, Main.SCREEN_HEIGHT/2 - 79, 400, 4);
+		selectedLabel.setVisible(false);
+		selectedLabel.setOpaque(true);
+		selectedLabel.setBackground(new Color(100, 73, 173)); //Purple
+		add(selectedLabel);
 		
 		startButton.setBounds(Main.SCREEN_WIDTH/2 + 200, 400, 400, 100);
 		startButton.setBorderPainted(false);
@@ -96,7 +99,7 @@ public class BeatIt extends JFrame {
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
-				startButton.setIcon(startButtonBasicImage);
+				startButton.setIcon(startButtonImage);
 				startButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
@@ -113,12 +116,18 @@ public class BeatIt extends JFrame {
 		quitButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				quitButton.setIcon(quitButtonEnteredImage);
+				if (isMainScreen)
+					quitButton.setIcon(quitButtonSquareEnteredImage);
+				else
+					quitButton.setIcon(quitButtonEnteredImage);
 				quitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
-				quitButton.setIcon(quitButtonBasicImage);
+				if (isMainScreen)
+					quitButton.setIcon(quitButtonSquareImage);
+				else
+					quitButton.setIcon(quitButtonImage);
 				quitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
@@ -141,7 +150,7 @@ public class BeatIt extends JFrame {
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
-				easyButton.setIcon(easyButtonBasicImage);
+				easyButton.setIcon(easyButtonImage);
 				easyButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
@@ -164,7 +173,7 @@ public class BeatIt extends JFrame {
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
-				hardButton.setIcon(hardButtonBasicImage);
+				hardButton.setIcon(hardButtonImage);
 				hardButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
@@ -175,19 +184,19 @@ public class BeatIt extends JFrame {
 		add(hardButton);
 		
 		backButton.setVisible(false);
-		backButton.setBounds(20, 50, 60, 60);
+		backButton.setBounds(Main.SCREEN_WIDTH - 120, 20, 100, 100);
 		backButton.setBorderPainted(false);
 		backButton.setContentAreaFilled(false);
 		backButton.setFocusPainted(false);
 		backButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				//backButton.setIcon(hardButtonEnteredImage);
+				backButton.setIcon(quitButtonSquareEnteredImage);
 				backButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
-				//backButton.setIcon(hardButtonBasicImage);
+				backButton.setIcon(quitButtonSquareImage);
 				backButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
@@ -196,35 +205,6 @@ public class BeatIt extends JFrame {
 			}
 		});
 		add(backButton);
-		
-		menuBar.setBounds(0, 0, 1280, 30);
-		menuBar.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mousePressed(MouseEvent e) {
-				mouseX = e.getX();
-				mouseY = e.getY();
-			}
-		});
-		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				int x = e.getXOnScreen();
-				int y = e.getYOnScreen();
-				setLocation(x - mouseX, y - mouseY);
-			}
-		});
-		add(menuBar);
-		
-		this.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e) {
-				if(e.getKeyCode() == KeyEvent.VK_UP) {
-					selectUp();
-				}
-				else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-					selectDown();
-				}
-			}
-		});
 	}
 	
 	public void paint(Graphics g) {
@@ -240,18 +220,18 @@ public class BeatIt extends JFrame {
 			g.drawImage(selectedImage, Main.SCREEN_WIDTH/4 - 150, Main.SCREEN_HEIGHT/2 - 200, null);
 		}
 		if(isGameScreen) {
-			g.drawImage(noteRouteImage, 332, 30, null);
-			g.drawImage(noteRouteImage, 436, 30, null);
-			g.drawImage(noteRouteImage, 540, 30, null);
-			g.drawImage(noteRouteImage, 640, 30, null);
-			g.drawImage(noteRouteImage, 744, 30, null);
-			g.drawImage(noteRouteImage, 848, 30, null);
-			g.drawImage(noteRouteLineImage, 328, 30, null);
-			g.drawImage(noteRouteLineImage, 432, 30, null);
-			g.drawImage(noteRouteLineImage, 536, 30, null);
-			g.drawImage(noteRouteLineImage, 740, 30, null);
-			g.drawImage(noteRouteLineImage, 844, 30, null);
-			g.drawImage(noteRouteLineImage, 948, 30, null);
+			g.drawImage(noteRouteImage, 332, 0, null);
+			g.drawImage(noteRouteImage, 436, 0, null);
+			g.drawImage(noteRouteImage, 540, 0, null);
+			g.drawImage(noteRouteImage, 640, 0, null);
+			g.drawImage(noteRouteImage, 744, 0, null);
+			g.drawImage(noteRouteImage, 848, 0, null);
+			g.drawImage(noteRouteLineImage, 328, 0, null);
+			g.drawImage(noteRouteLineImage, 432, 0, null);
+			g.drawImage(noteRouteLineImage, 536, 0, null);
+			g.drawImage(noteRouteLineImage, 740, 0, null);
+			g.drawImage(noteRouteLineImage, 844, 0, null);
+			g.drawImage(noteRouteLineImage, 948, 0, null);
 			g.drawImage(gameInfoImage, 0, 660, null);
 			g.drawImage(judgementLineImage, 0, 580, null);
 			g.setColor(Color.white);
@@ -269,45 +249,30 @@ public class BeatIt extends JFrame {
 	public void selectTrack(int nowSelected) {
 		if(selectedMusic != null)
 			selectedMusic.close();
+		selectedLabel.setBounds(Main.SCREEN_WIDTH - 400, Main.SCREEN_HEIGHT/2 + (nowSelected*125 - 175) + 96, 400, 4);
 		selectedImage = new ImageIcon(Main.class.getResource("../images/" + trackList.get(nowSelected).getTrackImage())).getImage();
 		selectedMusic = new Music(trackList.get(nowSelected).getSelectedMusic(), true);
 		selectedMusic.start();
 	}
 	
-	public void selectUp() {
-		if (nowSelected == 0)
-			nowSelected = trackList.size() - 1;
-		else
-			nowSelected--;
-		selectTrack(nowSelected);
-	}
-	
-	public void selectDown() {
-		if (nowSelected == trackList.size() - 1)
-			nowSelected = 0;
-		else
-			nowSelected++;
-		selectTrack(nowSelected);
-	}
-	
-	public void makeTrackButton(JButton trackButton, int trackNum, int y) {
-		trackButton.setBounds(Main.SCREEN_WIDTH - 400, Main.SCREEN_HEIGHT/2 + y, 400, 100);
+	public void makeTrackButton(JButton trackButton, int trackNum) {
+		trackButton.setBounds(Main.SCREEN_WIDTH - 400, Main.SCREEN_HEIGHT/2 + (trackNum*125 - 175), 400, 96);
 		trackButton.setBorderPainted(false);
 		trackButton.setFocusPainted(false);
-		trackButton.setBackground(new Color(129, 131, 135));
-		trackButton.setFont(new Font("Arial", Font.BOLD, 25));
+		trackButton.setBackground(new Color(240, 240, 240));
+		trackButton.setFont(new Font("Lucida Console", Font.BOLD, 25));
 		trackButton.setHorizontalAlignment(SwingConstants.LEFT);
 		trackButton.setVisible(false);
 		trackButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				trackButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-				trackButton.setBackground(new Color(0, 0, 0));
+				trackButton.setBackground(new Color(190, 190, 190));
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
 				trackButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-				trackButton.setBackground(new Color(129, 131, 135));
+				trackButton.setBackground(new Color(240, 240, 240));
 			}
 			@Override
 			public void mousePressed(MouseEvent e) {
@@ -330,13 +295,15 @@ public class BeatIt extends JFrame {
 		trackButton0.setVisible(false);
 		trackButton1.setVisible(false);
 		trackButton2.setVisible(false);
+		selectedLabel.setVisible(false);
 		//background = new ImageIcon(Main.class.getResource("../images/gameBackground.jpg")).getImage();
 	}
 	
 	public void enterMain() {
 		startButton.setVisible(false);
+		quitButton.setIcon(quitButtonSquareImage);
 		quitButton.setVisible(true);
-		quitButton.setBounds(20, 600, 400, 100);
+		quitButton.setBounds(Main.SCREEN_WIDTH - 120, 20, 100, 100);
 		background = new ImageIcon(Main.class.getResource("../images/mainBackground.jpg")).getImage();
 		isMainScreen = true;
 		isGameScreen = false;
@@ -346,6 +313,7 @@ public class BeatIt extends JFrame {
 		trackButton0.setVisible(true);
 		trackButton1.setVisible(true);
 		trackButton2.setVisible(true);
+		selectedLabel.setVisible(true);
 		introMusic.close();
 		selectTrack(0);
 	}
